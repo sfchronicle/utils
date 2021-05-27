f3f5038306e2b9c27a5a313e799ef50b8cc39215
David Deloso <daviddeloso2022@u.northwestern.edu>
2021-05-27 12:34:00 -0500

    nav update

--- specialnav.js
+++ specialnav.js
@@ -84,6 +84,28 @@ let getSpecialNav = function(meta, urlAdd, forceColor, navLink, navArray){
 		return subscribeLink
 	}
 	const subLink = getSubLink();
+	const getLinkHtml = (prefix) =>{
+		let htmlBlock = `<a class="special-nav-subscribe-link" href=${subLink} target="_blank">
+		<p>Subscribe</p>
+		</a>`
+		if(prefix == "ct"){
+			htmlBlock += `<a class="special-nav-newsletter-link" href="https://link.ctpost.com/join/signup-po" target="_blank">
+			<p>Subscribe</p>
+			</a>`
+		}
+		else if(prefix == "sf"){
+			htmlBlock += `<a class="special-nav-newsletter-link" href="https://link.sfchronicle.com/join/signup" target="_blank">
+			<p>Subscribe</p>
+			</a>`
+		}
+		else if(prefix == "hc"){
+			htmlBlock += `<a class="special-nav-newsletter-link" href="hhttps://link.houstonchronicle.com/join/signup-hc" target="_blank">
+			<p>Subscribe</p>
+			</a>`
+	}
+	return htmlBlock
+}
+	let navRightHtml = getSubLink(marketPrefix)
 	console.log(subLink)
 	let navHTML = `<nav class="topper-special-nav-container ${invertClass}">
 	<div class="special-nav-left">
@@ -113,9 +135,7 @@ let getSpecialNav = function(meta, urlAdd, forceColor, navLink, navArray){
 	</a>
 	</div>
 	<div class="special-nav-right">
-	<a class="special-nav-subscribe-link" href=${subLink} target="_blank">
-	<p>Subscribe</p>
-	</a>
+	${navHTML}
 	</div>
   </nav>`
 
