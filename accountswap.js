be82a9d430efd8149f14e2fbcb72985d8fa170d6
Evan Wagstaff <evanjwagstaff@gmail.com>
2022-08-16 10:42:00 -0400

    Getting ready to work on account swap

---
+++ accountswap.js
@@ -0,0 +1,10 @@
+
+// Detect if there's a valid signin -- if so, swap Subscribe button to Account button
+// Any domain + /realm/ should work for an account link
+const accountURL = "/realm/"
+
+const pollForAccount = function(){
+
+}
+
+module.exports = { pollForAccount }
