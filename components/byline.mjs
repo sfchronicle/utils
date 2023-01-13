03373050670fe18635a67b7664e77b6e436e24f7
leshuang-ht <lesley.huang@hearst.com>
2023-01-13 12:02:02 -0500

    Add new components from gatsby-newsapp

---
+++ components/byline.mjs
@@ -0,0 +1,71 @@
+import React, { Fragment } from 'react'
+import Authors from './authors.mjs'
+import ShareButtons from './sharebuttons.mjs'
+import {
+    pubdateString,
+    moddateString,
+} from './helpers/datehelpers.mjs'
+
+const Byline = ({ meta }) => {
+    const {
+        PROJECT: { AUTHORS, ISO_MODDATE, ISO_PUBDATE },
+      } = meta
+      const has_authors = AUTHORS[0].AUTHOR_NAME !== ""
+
+return (
+    <>
+        <div className="byline-wrapper">
+            <div className="byline">
+            {has_authors &&
+              <>
+              <span>By</span>
+              {AUTHORS.map((author, index) => {
+                // Pass special flag if this is the last item
+                let isLast = false
+                if (index === AUTHORS.length - 1) {
+                  isLast = true
+                }
+                // Add the bylines
+                return (
+                  <Authors
+                    key={author.AUTHOR_NAME}
+                    url={author.AUTHOR_PAGE}
+                    name={author.AUTHOR_NAME}
+                    index={index}
+                    isLast={isLast}
+                  />
+                )
+              })}
+              &nbsp;|&nbsp;
+              </>
+            }
+            {!has_authors && <span>Published </span>}
+              <time
+                className="topper-dateline"
+                dateTime={ISO_PUBDATE}
+                itemProp="datePublished"
+              >
+                {pubdateString}
+              </time>
+              {moddateString && (
+                <Fragment>
+                  &nbsp;|&nbsp;
+                  <time
+                    className="topper-dateline updated-date"
+                    dateTime={ISO_MODDATE}
+                    itemProp="dateModified"
+                  >
+                    Updated: {moddateString}
+                  </time>
+                </Fragment>
+              )}
+            </div>
+            <div id="sharebutton-wrapper">
+              <ShareButtons meta={meta} />
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default Byline
