943e7ce49da713276faf9f73686997d6f05e44f4
leshuang-ht <lesley.huang@hearst.com>
2023-02-08 22:23:04 -0500

    move scripts into utils

---
+++ components/layoutscript.mjs
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LayoutScript = () => {
+  return (
+    <>
+      <script src="https://projects.sfchronicle.com/shared/js/jquery.min.js"></script>
+      <script src="https://treg.hearstnp.com/treg.js"></script>
+    </>
+  )
+}
+
+export default LayoutScript
