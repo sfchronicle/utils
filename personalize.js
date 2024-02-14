293697d75a946e617583504061d76a20775ae5ad
Evan Wagstaff <evanjwagstaff@gmail.com>
2024-02-14 13:35:17 -0800

    If blueconic doesn't exist yet, try for 20 sec

--- personalize.js
+++ personalize.js
@@ -2,19 +2,34 @@
 const getProfileProperty = (property) => {
   return new Promise((resolve, reject) => {
     if (window) {
-      if (window.blueConicClient) {
-        const profile = window.blueConicClient.profile.getProfile();
-        const properties = [property];
-        profile.loadValues(properties, this, function () {
-          var value = profile.getValue(property);
-          // Return a valid result or null
-          value = value || null;
-          console.log("getProfileProperty VAL", value);
-          resolve(value);
-        });
-      } else {
-        resolve("Personalize: No BlueConic client found");
-      }
+      let attempts = 0;
+      const checkForBlueConic = (innerResolve) => {
+        if (window.blueConicClient) {
+          const profile = window.blueConicClient.profile.getProfile();
+          const properties = [property];
+          profile.loadValues(properties, this, function () {
+            var value = profile.getValue(property);
+            // Return a valid result or null
+            value = value || null;
+            console.log("getProfileProperty VAL", value);
+            innerResolve(value);
+          });
+        } else {
+          attempts++;
+          if (attempts < 20) {
+            // Check for BlueConic every second
+            setTimeout(() => {
+              checkForBlueConic(innerResolve);
+            }, 1000);
+          } else {
+            innerResolve(
+              "Personalize: No BlueConic client found after many attempts"
+            );
+          }
+        }
+      };
+      // Kick off check
+      checkForBlueConic(resolve);
     } else {
       resolve("Personalize: No window found");
     }
