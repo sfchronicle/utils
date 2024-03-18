1a62f9c3fe9bef0914bfc3b3bde2b08d760be5ab
Evan Wagstaff <evanjwagstaff@gmail.com>
2024-03-18 12:25:25 -0700

    Added a treg URL override option

--- components/layout/layoutscript.mjs
+++ components/layout/layoutscript.mjs
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const LayoutScript = () => {
+const LayoutScript = (domain) => {
+  let domainString = "";
+  if (domain) {
+    domain = domain.replace(".com", "");
+    domainString = `
+      var treg = treg || {};
+      treg.url_override="realm.${domain}.noux.com";
+    `;
+  }
   return (
     <>
       <script src="https://projects.sfchronicle.com/shared/js/jquery.min.js"></script>
+      {domainString && (
+        <script dangerouslySetInnerHTML={{ __html: domainString }}></script>
+      )}
       <script src="https://treg.hearstnp.com/treg.js"></script>
     </>
-  )
-}
+  );
+};
 
-export default LayoutScript
+export default LayoutScript;
