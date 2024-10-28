e3874fdab7426f6b40477ff4ba991b49d754ab89
Evan Wagstaff <evanjwagstaff@gmail.com>
2024-10-28 09:02:52 -0700

    Added gtm.js script at the request of Daniel M

--- components/helpers/utilfunctions.mjs
+++ components/helpers/utilfunctions.mjs
@@ -22,6 +22,17 @@ function appendLayoutScripts(isEmbedded, isAdRemoved, marketKey, category) {
   // React Helmet is actually terrible and runs these scripts twice, so we are including them async ourselves
   // Run analytics and resizing scripts right away so we take care of that
   if (!isEmbedded) {
+    // Google Tag Manager
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, "script", "dataLayer", "GTM-P38VLD8M");
     // Add new md5 script
     let script = document.createElement("script");
     script.type = "text/javascript";
