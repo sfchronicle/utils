6c9a7acb18bd48571937aa3cb08e08b54e0f8138
Evan Wagstaff <evanjwagstaff@gmail.com>
2026-05-18 15:21:43 -0700

    Add new relic browser error catching

--- components/layout/layoutscript.mjs
+++ components/layout/layoutscript.mjs
@@ -1,5 +1,12 @@
 import React from "react";
 
+const newRelicBrowserConfig = `
+;window.NREUM||(NREUM={});NREUM.init={distributed_tracing:{enabled:true},performance:{capture_measures:true},browser_consent_mode:{enabled:false},privacy:{cookies_enabled:true},ajax:{deny_list:["bam.nr-data.net"],capture_payloads:'none'}};
+
+;NREUM.loader_config={accountID:"3540744",trustKey:"41019",agentID:"626880230",licenseKey:"NRJS-bef5859a29fb3c538c2",applicationID:"626880230"};
+;NREUM.info={beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",licenseKey:"NRJS-bef5859a29fb3c538c2",applicationID:"626880230",sa:1};
+`;
+
 const LayoutScript = ({ domain, marketingVer = 1 }) => {
   let domainString = "";
   // Check if domain is string
@@ -17,7 +24,18 @@ const LayoutScript = ({ domain, marketingVer = 1 }) => {
   return (
     <>
       {marketingVer === 2 && (
-        <script src="https://d29h410fx4s4sw.cloudfront.net/marketing/marketing.umd.min.js"></script>
+        <>
+          <script src="https://d29h410fx4s4sw.cloudfront.net/marketing/marketing.umd.min.js"></script>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{ __html: newRelicBrowserConfig }}
+          />
+          <script
+            type="text/javascript"
+            src="https://js-agent.newrelic.com/nr-loader-spa-1.313.1.min.js"
+            crossOrigin="anonymous"
+          />
+        </>
       )}
       {domainString && (
         <script dangerouslySetInnerHTML={{ __html: domainString }}></script>
